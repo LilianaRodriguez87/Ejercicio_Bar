3dfc876ffc37d53d4c30664c9a8187c12ba0674c
lilianaRodriguez87 <liliana.erick33@gmail.com>
2021-03-08 23:05:47 -0500

    adicion

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function formulario (){
+  let NumerodeIdentificación,Nombres,apellidos,Edad,Email,Númerodetelefono,Númerodepersonas,inputState;
+NumerodeIdentificación = document.getElementById("NumerodeIdentificación").value;
+Nombres = document.getElementById("Nombres").value;
+apellidos = document.getElementById("apellidos").value;
+Edad = document.getElementById("Edad").value;
+Email = document.getElementById("Email").value;
+Númerodetelefono = document.getElementById("Númerodetelefono").value;
+Númerodepersonas = document.getElementById("Númerodepersonas").value;
+inputState = document.getElementById("inputState").value;
+
+if(Nombres === ""){
+    alert("El campo esta vacio");
+}
+}
+
+
+
